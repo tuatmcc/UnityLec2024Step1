3730499058fba3b53857a6eed1b3ffb00303c311
sugawa197203 <96975428+sugawa197203@users.noreply.github.com>
2024-07-20 17:46:44 +0900

    images

--- img/img.sh
+++ img/img.sh
@@ -2,6 +2,9 @@
 
 for file in *.png; do
   filename="${file%.*}"
+  if [ -f "$filename.webp" ]; then
+    continue
+  fi
   convert "$file" "$filename.webp"
   echo "Converted $file to $filename.webp"
 done
