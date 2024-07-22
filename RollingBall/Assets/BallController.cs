a1c20d9ad9d2d409a4295283a48c814dc4cc3384
sugawa197203 <96975428+sugawa197203@users.noreply.github.com>
2024-07-22 13:36:41 +0900

    ch 5 script

---
+++ RollingBall/Assets/BallController.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.W))
+		{
+			rb.AddForce(new Vector3(0, 0, 1));
+		}
+
+        if(Input.GetKey(KeyCode.S))
+        {
+            rb.AddForce(new Vector3(0, 0, -1));
+		}
+
+        if(Input.GetKey(KeyCode.A))
+		{
+			rb.AddForce(new Vector3(-1, 0, 0));
+        }
+
+		if(Input.GetKey(KeyCode.D))
+        {
+            rb.AddForce(new Vector3(1, 0, 0));
+		}
+    }
+}
